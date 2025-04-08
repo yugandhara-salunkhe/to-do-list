0b764809c8268a4f711f11c2fc64429752269e5d
yugandhara-salunkhe <yugandharasalunkhe49@gmail.com>
2025-04-08 11:37:18 +0530

    confetti commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const addTask = () => {
         updateTaskslist();
         updatestats();
         saveTasks();
-    };
+    }
 };
 
 const toogleTastcompleted = (index) => {
@@ -62,6 +62,10 @@ const updatestats = () =>{
     progressBar.style.width = `${'progress'}%`;
 
     document.getElementById('numbers').innerText =`${completetasks} / ${totalTasks}`;
+
+    if (tasks.length && completetasks === totalTasks){
+        blashconfetti();
+    }
 };
 
 const updateTaskslist = ()=> {
@@ -90,6 +94,49 @@ const updateTaskslist = ()=> {
 
     });
 };
+
+
+const blashconfetti = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
 
 document.getElementById("newTask").addEventListener("click", function (e) {
     e.preventDefault();
