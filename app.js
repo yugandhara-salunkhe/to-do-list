a0ceddec6d7ca918c7cda35d36b89876c692661a
yugandhara-salunkhe <yugandharasalunkhe49@gmail.com>
2025-04-08 11:27:36 +0530

    chng commit

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    const storedTasks =JSON.parse(localStorage.getItem("tasks"))
+
+    if(storedTasks){
+        storedTasks.forEach((task)=> tasks.push(task));
+        updateTaskslist();
+        updatestats();
+    }
+});
+
+
 let tasks = [];
+
+const saveTasks = () => {
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+};
+
 
 const addTask = () => {
     const taskInput = document.getElementById("taskInput");
@@ -8,7 +24,44 @@ const addTask = () => {
         tasks.push({ text: text, completed: false });
         taskInput.value = "";
         updateTaskslist();
-    }
+        updatestats();
+        saveTasks();
+    };
+};
+
+const toogleTastcompleted = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskslist();
+    updatestats();
+    saveTasks();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index,1);
+    updateTaskslist();
+    updatestats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = tasks[index].text
+
+    tasks.splice(index,1);
+    updateTaskslist();
+    updatestats();
+    saveTasks();
+};
+
+const updatestats = () =>{
+    const completetasks = tasks.filter((task)=> task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = (completetasks / totalTasks)*100;
+    const progressBar = document.getElementById('progress');
+
+    progressBar.style.width = `${'progress'}%`;
+
+    document.getElementById('numbers').innerText =`${completetasks} / ${totalTasks}`;
 };
 
 const updateTaskslist = ()=> {
@@ -22,13 +75,12 @@ const updateTaskslist = ()=> {
         <div class="taskItem">
           <div class="task ${task.completed ? "completed" : ""} ">
             <input type="checkbox" class="checkbox" ${
-                task.completed ? "checked" : ""
-            }/>
+                task.completed ? "checked" : "" }/>
             <p>${task.text}</p>
           </div>
           <div class="icons">
-            <img src="image/edit-button.png" onclick="editTask(${index})"/>
-            <img src="image/delete (1).png" onclick="deleteTask(${index})"/>
+            <img src="./image/edit-button.png" onclick="editTask(${index})"/>
+            <img src="./image/delete (1).png" onclick="deleteTask(${index})"/>
           </div>
         </div>
         `;
