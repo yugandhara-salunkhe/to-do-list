1cbaaec679e32ddfd9c84e3cd3eea47892a9a9e8
yugandhara-salunkhe <yugandharasalunkhe49@gmail.com>
2025-04-08 11:45:35 +0530

    cdn commit

--- app.js
+++ app.js
@@ -59,13 +59,13 @@ const updatestats = () =>{
     const progress = (completetasks / totalTasks)*100;
     const progressBar = document.getElementById('progress');
 
-    progressBar.style.width = `${'progress'}%`;
+    progressBar.style.width = `${progress}%`;
 
     document.getElementById('numbers').innerText =`${completetasks} / ${totalTasks}`;
 
     if (tasks.length && completetasks === totalTasks){
         blashconfetti();
-    }
+    };
 };
 
 const updateTaskslist = ()=> {
@@ -96,51 +96,51 @@ const updateTaskslist = ()=> {
 };
 
 
-const blashconfetti = () => {
-    const count = 200,
-  defaults = {
-    origin: { y: 0.7 },
-  };
-
-function fire(particleRatio, opts) {
-  confetti(
-    Object.assign({}, defaults, opts, {
-      particleCount: Math.floor(count * particleRatio),
-    })
-  );
-}
-
-fire(0.25, {
-  spread: 26,
-  startVelocity: 55,
-});
-
-fire(0.2, {
-  spread: 60,
-});
-
-fire(0.35, {
-  spread: 100,
-  decay: 0.91,
-  scalar: 0.8,
-});
-
-fire(0.1, {
-  spread: 120,
-  startVelocity: 25,
-  decay: 0.92,
-  scalar: 1.2,
-});
-
-fire(0.1, {
-  spread: 120,
-  startVelocity: 45,
-});
-}
-
 document.getElementById("newTask").addEventListener("click", function (e) {
     e.preventDefault();
 
     addTask();
 });
 
+const blashconfetti= () => {
+    const count = 200,
+      defaults = {
+        origin: { y: 0.7 },
+    };
+    
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio),
+        })
+      );
+    }
+    
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    
+    fire(0.2, {
+      spread: 60,
+    });
+    
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+    };
+    
